62a52823d11ea544bc90b69ba55fc93720f4bc0e
AFLAHA C <afiaflahac@gmail.com>
2026-08-19 10:57:43 +0530

    Initial CNN framework implementation

---
+++ evaluate.py
@@ -0,0 +1,3 @@
+def evaluate_model(model, test_data):
+    """Evaluate a trained model on test data."""
+    return {"model": model, "test_data": test_data, "score": None}
---
+++ uncertainty.py
@@ -0,0 +1,3 @@
+def compute_uncertainty(predictions):
+    """Return uncertainty values for predictions."""
+    return {"predictions": predictions, "uncertainty": None}
