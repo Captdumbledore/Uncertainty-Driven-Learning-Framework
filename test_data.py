50d6d87d2ff83db19236362f512ca68b6443f3c4
AFLAHA C <afiaflahac@gmail.com>
2026-09-02 12:25:35 +0530

    Add main guards to utility scripts

--- test_data.py
+++ test_data.py
@@ -1,15 +1,20 @@
 from data import get_dataloaders
 
 
-train_loader, validation_loader, test_loader, train_dataset = get_dataloaders(
-    batch_size=64
-)
+def main():
+    train_loader, validation_loader, test_loader, train_dataset = get_dataloaders(
+        batch_size=64
+    )
 
-images, labels = next(iter(train_loader))
+    images, labels = next(iter(train_loader))
 
-print("Images shape:", images.shape)
-print("Labels shape:", labels.shape)
+    print("Images shape:", images.shape)
+    print("Labels shape:", labels.shape)
 
-print("Number of training batches:", len(train_loader))
-print("Number of validation batches:", len(validation_loader))
-print("Number of test batches:", len(test_loader))
+    print("Number of training batches:", len(train_loader))
+    print("Number of validation batches:", len(validation_loader))
+    print("Number of test batches:", len(test_loader))
+
+
+if __name__ == "__main__":
+    main()
